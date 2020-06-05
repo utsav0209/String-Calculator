a477c3fa2a3c139f8b2c8054adc1859a58236804
utsav0209 <utsav99varia@gmail.com>
2020-06-05 15:12:16 +0530

    TEST: add test method for StringCalculator.add method

--- src/test/java/com/utsav0209/stringCalculator/StringCalculatorTest.java
+++ src/test/java/com/utsav0209/stringCalculator/StringCalculatorTest.java
@@ -1,5 +1,14 @@
 package com.utsav0209.stringCalculator;
 
+import static org.junit.Assert.assertEquals;;
+
+import org.junit.Test;
+
 public class StringCalculatorTest {
     
+    @Test
+    public void testAdd() {
+        assertEquals(0, StringCalculator.add(""));
+    }
+
 }
