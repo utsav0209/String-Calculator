2c2cc6dbf3b68b0093e95551294f4a4ba9854293
utsav0209 <utsav99varia@gmail.com>
2020-06-05 17:34:09 +0530

    FEAT: add exception class named NegativesNotAllowedException

---
+++ src/main/java/com/utsav0209/stringCalculator/NegativesNotAllowedException.java
@@ -0,0 +1,14 @@
+package com.utsav0209.stringCalculator;
+
+public class NegativesNotAllowedException extends Exception{
+    
+    /**
+	 * Auto generated serialVersionUID by IDE
+	 */
+	private static final long serialVersionUID = -1438524487448939071L;
+
+	NegativesNotAllowedException(String message){
+        super(message);
+    }
+
+}
