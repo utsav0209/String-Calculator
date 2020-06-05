d5159f050a03afdb3b17e4876a2b06d387c1ac77
utsav0209 <utsav99varia@gmail.com>
2020-06-05 18:06:23 +0530

    FEAT: added parameterized test class for NegativesNotAllowedException

---
+++ src/test/java/com/utsav0209/stringCalculator/NegativesNotAllowedExceptionTest.java
@@ -0,0 +1,49 @@
+package com.utsav0209.stringCalculator;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(Parameterized.class)
+public class NegativesNotAllowedExceptionTest {
+    
+    private String inputString;
+    private String expectedString;
+    private static String exceptionMessagePrefix = "negatives not allowed";
+
+    public NegativesNotAllowedExceptionTest(String inputString , String expectedString){
+        this.inputString = inputString;
+        this.expectedString = expectedString;
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> testCasesList() {
+        return Arrays.asList(new Object[][] {
+            { "-1", exceptionMessagePrefix + " " + "-1" },
+            { "-1,2", exceptionMessagePrefix + " " + "-1" },
+            { "1,-2,-3", exceptionMessagePrefix + " " + "-2" },
+        });
+    }
+
+    @Test(expected = NegativesNotAllowedException.class)
+    public void testNegtiveNumbers() throws NegativesNotAllowedException{
+        StringCalculator.add(inputString);
+    }
+
+    @Test
+    public void testNegtiveNumbersExceptionMessage() {
+        try {
+            StringCalculator.add(inputString);
+        } catch(NegativesNotAllowedException e){
+            assertEquals(expectedString, e.getMessage());
+        }
+    }
+
+}
