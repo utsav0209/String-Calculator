36b737de947919c30928c5e65513d6d668af9fa0
Utsav Varia <utsav99varia@gmail.com>
2020-06-05 12:59:39 +0530

    Initialized base maven project with JUnit

---
+++ src/main/java/com/utsav0209/stringCalculator/App.java
@@ -0,0 +1,13 @@
+package com.utsav0209.stringCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
