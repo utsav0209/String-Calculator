aa5c87153a3143f74c92909e3fd04c80100567f5
utsav0209 <utsav99varia@gmail.com>
2020-06-05 16:11:07 +0530

    ENHANCE: add handle \n as seperator for input

--- src/main/java/com/utsav0209/stringCalculator/StringCalculator.java
+++ src/main/java/com/utsav0209/stringCalculator/StringCalculator.java
@@ -11,8 +11,10 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }
+
+        String delimeter = ",|\n";
         
-        String[] numbersArray = numbers.split(",");
+        String[] numbersArray = numbers.split(delimeter);
         int sum = 0;
 
         for(String number : numbersArray){
