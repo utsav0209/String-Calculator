2ff13d7d47d29330ce104e9bbf98890d8700c472
utsav0209 <utsav99varia@gmail.com>
2020-06-05 20:33:36 +0530

    FEAT: add CLI support for StringCalculator

--- src/main/java/com/utsav0209/stringCalculator/App.java
+++ src/main/java/com/utsav0209/stringCalculator/App.java
@@ -1,13 +1,31 @@
 package com.utsav0209.stringCalculator;
 
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * App class for StringCalulator package
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.print("Enter string for addition: ");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        in.close();
+
+        input = input.replace("\\n", "\n");
+
+        StringCalculator stringCalculator = new StringCalculator();
+
+        int sum;
+		try {
+			sum = stringCalculator.add(input);
+            System.out.println("Sum: " + sum);
+		} catch (NegativesNotAllowedException e) {
+            System.out.println(e.getMessage());
+		}
+
     }
 }
