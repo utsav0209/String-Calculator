d964f317e1e702aadab600a5bfd7ccdbbed2afa9
utsav0209 <utsav99varia@gmail.com>
2020-06-05 15:14:14 +0530

    FEAT: add method definition for add inside StringCalculator

--- src/main/java/com/utsav0209/stringCalculator/StringCalculator.java
+++ src/main/java/com/utsav0209/stringCalculator/StringCalculator.java
@@ -1,5 +1,13 @@
 package com.utsav0209.stringCalculator;
 
 public class StringCalculator {
-    
+     /**
+     * 
+     * @param numbers numbers passed as string for addition 
+     * @return sum of the numbers
+     */
+
+    public static int add(String numbers){
+        return -1;
+    }
 }
