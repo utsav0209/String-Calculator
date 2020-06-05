abbe4d9e8a624ad286a58bf76412955e569aea77
utsav0209 <utsav99varia@gmail.com>
2020-06-05 15:23:49 +0530

    ENHANCE: add handle empty string add

--- src/main/java/com/utsav0209/stringCalculator/StringCalculator.java
+++ src/main/java/com/utsav0209/stringCalculator/StringCalculator.java
@@ -8,6 +8,9 @@ public class StringCalculator {
      */
 
     public static int add(String numbers){
+        if(numbers.isEmpty()){
+            return 0;
+        }
         return -1;
     }
 }
--- src/test/java/com/utsav0209/stringCalculator/StringCalculatorTest.java
+++ src/test/java/com/utsav0209/stringCalculator/StringCalculatorTest.java
@@ -1,6 +1,6 @@
 package com.utsav0209.stringCalculator;
 
-import static org.junit.Assert.assertEquals;;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
