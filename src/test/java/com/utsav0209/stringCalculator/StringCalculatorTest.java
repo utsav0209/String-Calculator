ba0d2fa3363faec93138b4501a40b8a9c34835ef
utsav0209 <utsav99varia@gmail.com>
2020-06-05 15:40:32 +0530

    REFACTOR: test class Parameterized

--- src/test/java/com/utsav0209/stringCalculator/StringCalculatorTest.java
+++ src/test/java/com/utsav0209/stringCalculator/StringCalculatorTest.java
@@ -1,15 +1,40 @@
 package com.utsav0209.stringCalculator;
 
-import static org.junit.Assert.assertEquals;
+import java.util.Collection;
+import java.util.Arrays;
 
 import org.junit.Test;
 
+import org.junit.runners.Parameterized;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(Parameterized.class)
 public class StringCalculatorTest {
     
+    private String inputString;
+    private int expectedInteger;
+
+
+    public StringCalculatorTest(String inputString, int expectedInteger){
+        this.inputString = inputString;
+        this.expectedInteger = expectedInteger;
+    }
+
+    @Parameterized.Parameters
+    public static Collection primeNumbers() {
+       return Arrays.asList(new Object[][] {
+          { "", 0 },
+          { "1", 1 }
+       });
+    }
+
+
     @Test
     public void testAdd() {
-        assertEquals(0, StringCalculator.add(""));
-        assertEquals(1, StringCalculator.add("1"));
+        assertEquals(expectedInteger, StringCalculator.add(inputString));
     }
 
 }
